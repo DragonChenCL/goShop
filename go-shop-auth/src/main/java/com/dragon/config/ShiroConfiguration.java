577c584b667d87757a4e3d7bd57795a3d5f431e8
chenlong <chenlong_qrr@163.com>
2019-11-28 22:28:43 +0800

    init

--- go-shop-auth/src/main/java/com/dragon/config/ShiroConfiguration.java
+++ go-shop-auth/src/main/java/com/dragon/config/ShiroConfiguration.java
@@ -1,9 +1,14 @@
 package com.dragon.config;
 
 import com.dragon.realm.CustomRealm;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,14 +26,14 @@ public class ShiroConfiguration {
         shiroFilterFactoryBean.setUnauthorizedUrl("/notRole");
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-        filterChainDefinitionMap.put("/api/swagger-ui.html", "anon");
-        filterChainDefinitionMap.put("/api/swagger-resources", "anon");
-        filterChainDefinitionMap.put("/api/v2/api-docs", "anon");
-        filterChainDefinitionMap.put("/api/webjars/springfox-swagger-ui/**", "anon");
-        filterChainDefinitionMap.put("/api/configuration/security", "anon");
-        filterChainDefinitionMap.put("/api/configuration/ui", "anon");
+//        filterChainDefinitionMap.put("/api/swagger-ui.html", "anon");
+//        filterChainDefinitionMap.put("/swagger-resources", "anon");
+//        filterChainDefinitionMap.put("/v2/api-docs", "anon");
+//        filterChainDefinitionMap.put("/webjars/springfox-swagger-ui/**", "anon");
+//        filterChainDefinitionMap.put("/configuration/security", "anon");
+//        filterChainDefinitionMap.put("/configuration/ui", "anon");
         //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截 剩余的都需要认证
-        filterChainDefinitionMap.put("/**", "authc");
+        filterChainDefinitionMap.put("/**", "anon");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return shiroFilterFactoryBean;
     }
@@ -37,11 +42,41 @@ public class ShiroConfiguration {
     public SecurityManager securityManager(){
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
         defaultWebSecurityManager.setRealm(customRealm());
+        //关闭shiro自带session
+        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+        defaultWebSecurityManager.setSubjectDAO(subjectDAO);
         return defaultWebSecurityManager;
     }
 
     @Bean
     public CustomRealm customRealm(){
         return new CustomRealm();
+    }
+
+    /**
+     * 添加注解支持
+     */
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        // 强制使用cglib，防止重复代理和可能引起代理出错的问题
+        // https://zhuanlan.zhihu.com/p/29161098
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
     }
 }
