742890447e02220500450819cd49faca0447fbd6
chenlong <chenlong_qrr@163.com>
2019-12-18 22:30:25 +0800

    init

--- go-shop-cache/src/main/java/com/dragon/redis/config/RedisConfig.java
+++ go-shop-cache/src/main/java/com/dragon/redis/config/RedisConfig.java
@@ -1,4 +1,13 @@
 package com.dragon.redis.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
 public class RedisConfig {
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(){
+        return null;
+    }
 }
